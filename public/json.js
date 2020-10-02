15da89081da6edae7b65e114ed2f0b724dfe7bc4
Pedro Mendoza <luispedro_mendoza@ucol.mx>
2020-10-01 23:19:53 -0500

    Version 2

--- public/json.js
+++ public/json.js
@@ -1,9 +1,6 @@
 window.onload = () => {fetch("http://localhost:3000/api/users/create", {
    method: "POST",
    cache: "no-cache",
-   headers: {
-      'Content-Type': 'application/json'
-   },
    body: {
       "id": 3,
       "name": "Diana Arceo Suarez",
