97de817fd580974cd2160221f91a1f482609ce04
Pedro Mendoza <luispedro_mendoza@ucol.mx>
2020-10-02 13:26:45 -0500

    Update and delete

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { join } = require('path');
-
+const _ = require('underscore');
+const { format } = require('path');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -65,6 +65,69 @@ app.post("/api/users/create", async (req, res) => {
    }
 });
 
+app.delete("/api/users/delete/:id", async (req, res) => {
+try {
+      let users = await fs.readFileSync(path.join(__dirname, '/data/users'), {
+         encoding: "utf-8"
+      })
+
+      users = JSON.parse(users);
+      for(let i = 0; i < users.length; i++) {
+         if(users[i].id == req.params.id) {
+            users.splice(i, 1);
+         }
+         break;
+      }
+      await fs.writeFileSync(path.join(__dirname, '/data/users'), JSON.stringify(users));
+
+      res.setHeader("statusCode", 200);
+      res.setHeader("content-type", "text/json");
+      res.json({
+         error: false,
+         "message": "Usuario eliminado correctamente"
+      });
+
+   } catch (error) {
+      console.log(error)
+      res.setHeader("statusCode", 500);
+      res.setHeader("content-type", "text/json");
+      res.json({
+         "message": "Hubo un error"
+      });
+   }
+});
+
+app.post("/api/users/update", async (req, res) => {
+   try {
+      let users = await fs.readFileSync(path.join(__dirname, '/data/users'), {
+         encoding: "utf-8"
+      })
+
+      users = JSON.parse(users);
+      for (let i = 0; i < users.length; i++) {
+         if (users[i].id == req.body.id) {
+            users[i] = req.body;
+            break;
+         }
+      }
+      await fs.writeFileSync(path.join(__dirname, '/data/users'), JSON.stringify(users));
+
+      res.setHeader("statusCode", 200);
+      res.setHeader("content-type", "text/json");
+      res.json({
+         error: false,
+         "message": "Usuario actualizado correctamente"
+      });
+
+   } catch (error) {
+      console.log(error)
+      res.setHeader("statusCode", 500);
+      res.setHeader("content-type", "text/json");
+      res.json({
+         "message": "Hubo un error"
+      });
+   }
+});
 
 app.listen(PORT, () => {
    console.log(`Server listening on port ${PORT}`)
