a0b88a9199ee5e1e70bc3e2f0d4d043793bf8b95
Pedro Mendoza <luispedro_mendoza@ucol.mx>
2020-10-01 22:36:07 -0500

    Version uno api-prueba

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { join } = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+
+
+app.get("", (req, res) => {
+   res.setHeader('statusCode', 200);
+   res.setHeader("content-type", "text/html");
+   res.send("<h1>Holaaa</h1>");
+});
+
+app.get("/api/users", async (req, res) => {
+   try {
+      let users = await fs.readFileSync(path.join(__dirname, '/data/users'), {encoding: "utf-8"});
+      users = JSON.parse(users);
+      res.setHeader("statusCode", 200);
+      res.setHeader("content-type", "text/json");
+      res.json({users, error: false, "message": "Usuarios obtenidos"});
+   }catch(err) {
+      res.setHeader("statusCode", 500);
+      res.setHeader("content-type", "text/json");
+      res.json({
+         "message": "Hubo un error"
+      });
+   }
+});
+
+app.post("/api/users/create", async (req, res) => {
+
+   try {
+      let users = await fs.readFileSync(path.join(__dirname, '/data/users'), {
+         encoding: "utf-8"
+      })
+
+      users = JSON.parse(users);
+      users.push(req.body.user)
+      await fs.writeFileSync(path.join(__dirname, '/data/users'), JSON.stringify(users));
+
+      res.setHeader("statusCode", 200);
+      res.setHeader("content-type", "text/json");
+      res.json({
+         users,
+         error: false,
+         "message": "Usuario agregado correctamente"
+      });
+
+   } catch (error) {
+      console.log(error)
+      res.setHeader("statusCode", 500);
+      res.setHeader("content-type", "text/json");
+      res.json({
+         "message": "Hubo un error"
+      });
+   }
+});
+
+
+app.listen(PORT, () => {
+   console.log(`Server listening on port ${PORT}`)
+});
